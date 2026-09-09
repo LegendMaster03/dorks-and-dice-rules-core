c50f890bb4d6c48cf9ad0200003391b45a1e3c39
Kyle Barnett <kbarnett0306@gmail.com>
2026-09-08 22:37:05 -0400

    Install source access administration module

--- src/RulesCore.Web/wwwroot/app.js
+++ src/RulesCore.Web/wwwroot/app.js
@@ -2,6 +2,7 @@ import { RulesCoreApi, loadToolHostContext } from "./api.js";
 import { RulesAuthoringApp } from "./authoring.js";
 import { installCampaignBaselineAuthoring } from "./campaign-baseline-authoring.js";
 import { installConceptSourceAuthoring } from "./concept-source-authoring.js";
+import { installSourceAccessAdministration } from "./source-access-admin.js";
 import { installSourceAdministration } from "./source-admin.js";
 import { alertNode, clear, describeError, element } from "./ui.js";
 
@@ -29,6 +30,7 @@ try {
     const app = new RulesAuthoringApp(root, api, hostContext, session, campaigns);
     installConceptSourceAuthoring(app);
     installSourceAdministration(app);
+    installSourceAccessAdministration(app);
     installCampaignBaselineAuthoring(app);
     await app.render();
 } catch (error) {
